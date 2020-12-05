8728b02ef8052630040f12143db9912c98e4dde6
E2029-Aydin <aydinsanoz2@gmail.com>
2020-12-05 19:33:30 +0300

    css style added

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
-var cookieParser = require ("cookie-parser")
-const logger = require("morgan");
 const port = process.env.port || 5001;
 const host = "localhost";
 const app = express();
 //middleware
-app.use(logger("dev"));
-
-
-app.use((req, res, next) => { //Authentication
-    console.log("Auth");
-    console.log("Cookies: ", req.signedCookies);
-    // some jobs
-    res.isAuth = false;
-    next();
-  });
-  app.use((req, res, next) => {
-    console.log("Controller // db");
-   if (res.isAuth) {
-       console.log("Private data")
-   } else {
-       console.log("not auth")
-   }
-    next();
-  });
-  app.use((req, res, next) => {
-    console.log("middleware: 3");
-    if (true) {
-      res.send("send");
-    }
-  });
-
-  app.listen(port, ()=>{
-      console.log("I am listenin on %s:%s",host,port)
-  })
+app.use(express.static(__dirname + "/public"))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+app.get("/", (req, res) => {
+    res.render("index");
+})
+app.listen(port, host, () => {
+    console.log("I'm listening on http://%s:%s", host, port);
+})
